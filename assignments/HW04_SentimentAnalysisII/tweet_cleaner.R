727e02091c97140d3c4d0abbff810592c8e47276
juliaparish <jparish@bren.ucsb.edu>
2022-04-23 16:23:57 -0700

    struggling thru Q1

---
+++ assignments/HW04_SentimentAnalysisII/tweet_cleaner.R
@@ -0,0 +1,15 @@
+# tweet cleaner function
+
+tweet_cleaner <- function(x)
+  {gsub("http[[:alnum:]]*",'', x)
+  gsub('http\\S+\\s*', '', x) ## Remove URLs
+  gsub('\\b+RT', '', x) ## Remove RT
+  gsub('#\\S+', '', x) ## Remove Hashtags
+  gsub('@\\S+', '', x) ## Remove Mentions
+  gsub('[[:cntrl:]]', '', x) ## Remove Controls and special characters
+  gsub("\\d", '', x) ## Remove Controls and special characters
+  gsub('[[:punct:]]', '', x) ## Remove Punctuations
+  gsub("^[[:space:]]*","",x) ## Remove leading whitespaces
+  gsub("[[:space:]]*$","",x) ## Remove trailing whitespaces
+  gsub(' +',' ',x) ## Remove extra whitespaces
+}
